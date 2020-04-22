35a452aa67cfc5984d11987b29c87beb70d2c655
Arkadiusz <arkadiusz@ostrowski.co>
2020-04-22 15:59:42 +0100

    hero section loading up for mobile and desktop - no background images on desktop yet - align btn size with the supplied design - style most of copy and image for desktop

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ const menuBtn = document.getElementById('menu-btn');
 const menuItems = document.getElementById('menu-items');
 const mobileMenuIcons = document.getElementsByClassName('header--mobile-menu-icon');
 const specialMenuBtn = document.getElementById('special-menu-btn');
+const heroContainer = document.getElementById('hero-container');
 
 menuBtn.onclick = () => {
     menuItems.classList.toggle('header--menu-items__show');
@@ -12,11 +13,29 @@ menuBtn.onclick = () => {
     }
 };
 
+window.onload = () => {
+  if(window.innerWidth >= 650) {
+    // render appropriate button for mobile or desktop menu when loading page
+    if(specialMenuBtn.classList.contains('btn-menu')) {
+      specialMenuBtn.classList.remove('btn-menu');
+      specialMenuBtn.classList.add('btn-primary');
+    }
+  }
+
+  if(window.innerWidth <= 850) {
+    // render hero section without container class
+    if(heroContainer.classList.contains('container')) {
+      heroContainer.classList.remove('container');
+    }
+  }
+};
+
 window.onresize = () => {
   // automatically close open mobile menu if user switches window size to tablet or higher
   if(window.innerWidth >= 650) {
     if(menuItems.classList.contains('header--menu-items__show')) {
       menuItems.classList.toggle('header--menu-items__show');
+      // on btn click swap active mobile menu icon
       for(let icon of mobileMenuIcons) {
         icon.classList.toggle('header--mobile-menu-icon__active');
       }
@@ -33,14 +52,16 @@ window.onresize = () => {
       specialMenuBtn.classList.add('btn-menu');
     }
   }
-};
 
-window.onload = () => {
-  if(window.innerWidth >= 650) {
-    // render appropriate button for mobile or desktop menu when loading page
-    if(specialMenuBtn.classList.contains('btn-menu')) {
-      specialMenuBtn.classList.remove('btn-menu');
-      specialMenuBtn.classList.add('btn-primary');
+  if(window.innerWidth >= 850) {
+    // render hero section with container class on desktop
+    if(!heroContainer.classList.contains('container')) {
+      heroContainer.classList.add('container');
+    }
+  } else {
+    // render hero section without container class on tablet and smaller devices
+    if(heroContainer.classList.contains('container')) {
+      heroContainer.classList.remove('container');
     }
   }
 };
